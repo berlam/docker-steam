8f4abe58924fff2ebcff094ee898deff9ee2a10d
Matthias Berla <matthias@berla.net>
2016-07-06 14:21:10 +0200

    Improve performance again

--- steam.sh
+++ steam.sh
@@ -2,6 +2,10 @@
 GENERAL="
 -v /etc/localtime:/etc/localtime:ro \
 -v /etc/machine-id:/etc/machine-id:ro \
+-v /etc/passwd:/etc/passwd:ro \
+-v /etc/shadow:/etc/shadow:ro \
+-v /etc/sudoers.d:/etc/sudoers.d:ro \
+--user=$(id -u) \
 --ipc='host' \
 --net='host' \
 "
@@ -9,6 +13,7 @@ STORAGE="
 -v $HOME/steam:/home/steam \
 "
 GRAPHIC="
+-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 -e DISPLAY=$DISPLAY \
 $(find /dev/dri/ -type c | sed 's/^/--device /') \
 "
@@ -16,4 +21,4 @@ SOUND="
 -v /etc/asound.conf:/etc/asound.conf \
 $(find /dev/snd/ -type c | sed 's/^/--device /') \
 "
-docker run --rm --security-opt=seccomp:unconfined --name="steam" $GENERAL $STORAGE $GRAPHIC $SOUND berla/steam
+docker run -d --security-opt=seccomp:unconfined --name="steam" $GENERAL $STORAGE $GRAPHIC $SOUND berla/steam
